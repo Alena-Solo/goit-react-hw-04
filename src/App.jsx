85034c257eee6cac811e777b772ae83f7ab07275
Alena-Solo <bazarkina.alena2020@gmail.com>
2025-01-02 16:22:15 +0100

    app

---
+++ src/App.jsx
@@ -0,0 +1,98 @@
+import { useEffect, useState } from "react";
+import { fetchImagesWithPhoto } from "./components/images-api";
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
+import ImageCard from "./components/ImageCard/ImageCard";
+import ImageGallery from "./components/ImageGallery/ImageGallery";
+import ImageModal from "./components/ImageModal/ImageModal";
+import Loader from "./components/Loader/Loader";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
+import SearchBar from "./components/SearchBar/SearchBar";
+
+export default function App() {
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [searchQuery, setSearchQuery] = useState(" ");
+  const [noResults, setNoResults] = useState(false);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const hendleSearch = async (imageName) => {
+    setSearchQuery(imageName);
+    setPage(1);
+    setImages([]);
+  };
+
+  useEffect(() => {
+    if (!searchQuery) return;
+
+    const fetchImages = async () => {
+      try {
+        setDefaultResultOrder(false);
+        setLoading(true);
+        setNoResults(false);
+        const data = await fetchImagesWithPhoto(searchQuery, page);
+        if (data.results.length === 0) {
+          setNoResults(true);
+          return;
+        }
+        setImages((prevImages) => [...prevImages, ...data.results]);
+
+        setTotalPages(data.total_pages);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchImages();
+  }, [searchQuery, page]);
+
+  const loadMore = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  const openModal = (image, index) => {
+    setModalIsOpen(true);
+
+    setSelectedImage(image);
+    setCurrentIndex(index);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+
+    setSelectedImage(null);
+  };
+
+  return (
+    <div>
+      <SearchBar onSearch={handleSearch} />
+      {noResults && (
+        <ErrorMessage children={"No photos Found matching your request"} />
+      )}
+      {images.length > 0 && (
+        <ImageGallery images={images} onClick={openModal} />
+      )}
+      {loading && <Loader />}
+      {error && (
+        <ErrorMessage children={"Something go wrong, please try again!"} />
+      )}
+      {images.length > 0 && page < totalPages && (
+        <LoadMoreBtn onClick={loadMore} />
+      )}
+      {selectedImage && (
+        <ImageModal
+          images={images}
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          currentIndex={currentIndex}
+          setCurrentIndex={setCurrentIndex}
+        />
+      )}
+    </div>
+  );
+}
